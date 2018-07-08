dd610c8a22d691a4fd3de45896f3873344b0338e
Unknown <mateo2096@hotmail.es>
2018-07-08 18:39:38 -0500

    Completed

--- chat-getting-started/src/main/java/io/axoniq/labs/chat/commandmodel/ChatRoom.java
+++ chat-getting-started/src/main/java/io/axoniq/labs/chat/commandmodel/ChatRoom.java
@@ -1,7 +1,88 @@
 package io.axoniq.labs.chat.commandmodel;
 
+import io.axoniq.labs.chat.coreapi.CreateRoomCommand;
+import io.axoniq.labs.chat.coreapi.JoinRoomCommand;
+import io.axoniq.labs.chat.coreapi.LeaveRoomCommand;
+import io.axoniq.labs.chat.coreapi.MessagePostedEvent;
+import io.axoniq.labs.chat.coreapi.ParticipantJoinedRoomEvent;
+import io.axoniq.labs.chat.coreapi.ParticipantLeftRoomEvent;
+import io.axoniq.labs.chat.coreapi.PostMessageCommand;
+import io.axoniq.labs.chat.coreapi.RoomCreatedEvent;
+import org.axonframework.commandhandling.CommandHandler;
+import org.axonframework.commandhandling.model.AggregateIdentifier;
+import org.axonframework.eventsourcing.EventSourcingHandler;
+import org.axonframework.spring.stereotype.Aggregate;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.axonframework.commandhandling.model.AggregateLifecycle.apply;
+
+@Aggregate
 public class ChatRoom {
 
-    // TODO: This class has just been created to make the test compile. It's missing, well, everything...
+    @AggregateIdentifier
+    private String roomId;
 
+    private Set<String> participants;
+
+    /*
+    * Separate the decision making from the state changes!
+    */
+
+    public ChatRoom() {
+        this.participants = new HashSet<>();
+    }
+
+    @CommandHandler
+    public ChatRoom(CreateRoomCommand cmd) {
+
+        this();
+
+        // Invokes the event handlers in this chatroom instance (which take care of the state changes)
+        // and schedules the event for publication
+        apply(new RoomCreatedEvent(cmd.getRoomId(), cmd.getName()));
+    }
+
+    @CommandHandler
+    public void handle(JoinRoomCommand cmd) {
+
+        if(!participants.contains(cmd.getParticipant())) {
+            apply(new ParticipantJoinedRoomEvent(cmd.getParticipant(), cmd.getRoomId()));
+        }
+    }
+
+    @CommandHandler
+    public void handle(LeaveRoomCommand cmd) {
+
+        if(participants.contains(cmd.getParticipant())) {
+            apply(new ParticipantLeftRoomEvent(cmd.getParticipant(), cmd.getRoomId()));
+        }
+    }
+
+    @CommandHandler
+    public void handle(PostMessageCommand cmd) {
+
+        if(!participants.contains(cmd.getParticipant())) {
+
+            throw new IllegalStateException("You must join a room before posting!");
+        }
+
+        apply(new MessagePostedEvent(cmd.getParticipant(), cmd.getRoomId(),cmd.getMessage()));
+    }
+
+    @EventSourcingHandler
+    public void on(RoomCreatedEvent event) {
+        this.roomId = event.getRoomId();
+    }
+
+    @EventSourcingHandler
+    public void on(ParticipantJoinedRoomEvent event) {
+        participants.add(event.getParticipant());
+    }
+
+    @EventSourcingHandler
+    public void on(ParticipantLeftRoomEvent event) {
+        participants.remove(event.getParticipant());
+    }
 }
